31652f6fef0c2a44e55c335843e4d289fefc5427
Step1017 <s.aveta@yahoo.com>
2023-03-01 15:11:07 +0100

    Query 3

--- queries.sql
+++ queries.sql
@@ -7,3 +7,8 @@ WHERE `date_of_birth` LIKE '1990%';
 SELECT * 
 FROM `courses` 
 WHERE `cfu` > 10;
+
+/* 3. Selezionare tutti gli studenti che hanno più di 30 anni */
+SELECT * 
+FROM `students` 
+WHERE YEAR(CURRENT_DATE) - YEAR(`date_of_birth`) > 30;
