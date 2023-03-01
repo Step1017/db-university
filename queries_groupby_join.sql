7d0a1db83b482ccf45e8c0faaf150acbfa4989e1
Step1017 <s.aveta@yahoo.com>
2023-03-01 17:48:39 +0100

    Query 2 Join

--- queries_groupby_join.sql
+++ queries_groupby_join.sql
@@ -27,3 +27,11 @@ FROM `students`
 JOIN `degrees`
 ON `degrees`.`id` = `students`.`degree_id`
 WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
+
+/* 2. Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze */
+SELECT `degrees`.`name`
+FROM `degrees`
+JOIN `departments`
+ON `department_id` = `degrees`.`department_id`
+WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
+
