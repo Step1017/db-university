1beec5e5d5824f39ba03bf3bd5d16ef6717bded5
Step1017 <s.aveta@yahoo.com>
2023-03-03 14:52:03 +0100

    Query 3 Join

--- queries_groupby_join.sql
+++ queries_groupby_join.sql
@@ -32,6 +32,16 @@ WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
 SELECT `degrees`.`name`
 FROM `degrees`
 JOIN `departments`
-ON `department_id` = `degrees`.`department_id`
+ON `department_id` = `degrees`.`departments_id`
 WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
+
+/* 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44) */
+SELECT courses.id, courses.name, courses.period, courses.year, courses.cfu, courses.degree_id, course_teacher.teacher_id
+FROM `courses`
+JOIN course_teacher
+ON courses.id = course_teacher.course_id
+JOIN teachers
+ON course_teacher.teacher_id = teachers.id
+WHERE teachers.name = 'Fulvio'
+AND teachers.surname = 'Amato';
 
